341be6ef95cbed4759f3297bfd01bc4e2e1d9c6e
stantonxu <xjiefeng@hotmail.com>
2017-10-07 22:06:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# hello-world
+My project following the github manual.
+
+Hi all,
+
+This edit is done by Stanton Xu, the only developer for this project so far.
---
+++ hello-world/src/main/java/com/stanton/maven/hello_world/Desktop.java
@@ -0,0 +1,36 @@
+package com.stanton.maven.hello_world;
+
+public class Desktop {
+	
+	private String name;
+	private String address;
+
+	public Desktop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Desktop(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+	
+	
+
+}
